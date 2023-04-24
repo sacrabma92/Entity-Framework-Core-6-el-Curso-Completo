e297d74cad6f1b9f4ebce1a778a02697b6455de7
Carlos Ramirez <karlossander92@gmail.com>
2023-04-23 20:05:42 -0500

    Empezando la vista

---
+++ CodeFirst/CursoEntityCore/Controllers/CategoriasController.cs
@@ -0,0 +1,24 @@
+﻿using CursoEntityCore.Datos;
+using CursoEntityCore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CursoEntityCore.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        public readonly ApplicationDbContext _context;
+        public CategoriasController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            List<Categoria> listaCategorias = _context.Categoria.ToList();
+            return View(listaCategorias);
+        }
+
+
+    }
+}
